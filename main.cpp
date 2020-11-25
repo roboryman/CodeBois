cf812e9207743f81bd28e71ae1c05270fb4927b4
roboryman <merrick.ryman@ufl.edu>
2020-11-25 05:36:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+Project
+    Visualizing the Spread of COVID-19 in the United States
+Contributers
+    Austin Padilla
+    Frank Olivera
+    Merrick Ryman
+*/
+
+int main() {
+  std::cout << "Hello World!\n";
+}
